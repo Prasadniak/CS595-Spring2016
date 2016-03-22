ce4b9df5a03d7bd5980cd395add52867ac9947cf
Prasad Niak <prasadniak@gmail.com>
2016-03-22 23:53:19 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
  devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :users do
-    resource :profile
-  end
   resources :contacts
   root 'pages#home'
   get '/about' => 'pages#about'
